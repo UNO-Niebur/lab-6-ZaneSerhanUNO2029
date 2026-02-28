6cfb69b299f2ffe2809d9dc4a4fbb0e2a4cbfff3
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-02-28 19:08:27 +0000

    Completed Lab6 WordCount

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,35 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Zane Serhan
+#Date: 2/28/2026
+#Assignment: Lab6 WordCount
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
+  while True:
+
+    filename = input("Enter filename (gettysberg.txt or fish.txt): ")
+    if filename == "gettysberg.txt" or filename == "fish.txt":
+      textFile = open(filename, 'r')
   
-  for line in textFile:
-    print(line)
-  
+      lineCount = 0
+      wordCount = 0
+      charCount = 0
+
+      for line in textFile:
+        lineCount = lineCount + 1
+        words = line.split()
+        for w in words:
+          wordCount = wordCount + 1
+        charCount += len(line)
+
+      print("Lines:", lineCount)
+      print("Words", wordCount)
+      print("Characters including Spaces/Newlines:", charCount )
+      
+      break
+
+    else:
+      print("Error: Invalid filename. Please try again with gettysberg.txt or fish.txt.")
+    
 
 if __name__ == '__main__':
   main()
